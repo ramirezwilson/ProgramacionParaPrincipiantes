ccd45da3f84a6dfbbcf6d1f6d708e6713cb31880
Wilson M. Ramirez F <ramirezwilson20@gmail.com>
2023-12-14 23:40:52 -0300

    add file if-else.ts

---
+++ sections/if-else.ts
@@ -0,0 +1,21 @@
+console.log('Inicio de programa');
+
+export let isTired = !true;
+
+// if (isTired) {
+//     console.log('Tomare un cafe');
+// } else {
+//     console.log('Tomar agua');
+// }
+
+let grade:number = 35;
+
+if (grade >= 60) {
+    console.log('El alumno aprueba la clase');
+} else if (grade > 50) {
+    console.log('Por favor estudia mas');
+} else {
+    console.log('El alumno No aprueba la clase');
+}
+
+console.log('Fin de programa');
