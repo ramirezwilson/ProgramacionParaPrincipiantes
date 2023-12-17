373e1e51ec126e8c6ed0a9302123dd25cef63497
Wilson M. Ramirez F <ramirezwilson20@gmail.com>
2023-12-16 21:39:11 -0300

    add file while-do-while.ts

---
+++ sections/while-do-while.ts
@@ -0,0 +1,16 @@
+//ciclo while
+/* let  gasTank:number = 50;
+while ( gasTank >= 0) {
+    console.log(gasTank);
+    gasTank --;
+}
+console.log('Tanke Agotado'); */
+
+// ciclo Do while
+export let  gasTank:number = 50;
+
+do {
+    console.log(gasTank);
+    gasTank --;
+} while (gasTank > 0);
+console.log('Tanke Agotado');
