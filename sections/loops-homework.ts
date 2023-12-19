44346246fdbfb88e77e33120b3105542be24ae5a
Wilson M. Ramirez F <ramirezwilson20@gmail.com>
2023-12-19 04:34:07 -0300

    add file loops-homework.ts

---
+++ sections/loops-homework.ts
@@ -0,0 +1,23 @@
+// let base: number = 10;
+// let limit: number = 50;
+
+// for (let i = 1; i <= limit; i++) {
+//     console.log(base,' x ', i, ' = ', base*i);
+    
+// }
+
+
+export let base: number = 10;
+export let limit: number = 50;
+export let i: number = 1;
+
+
+// while ( i <= limit) {
+    // console.log(base,' x ', i, ' = ', base* i);
+    // i++;
+// }
+
+do {
+    console.log(base,' x ', i, ' = ', base* i);
+    i++;
+} while (i <= limit);
