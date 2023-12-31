c3a951b09cc35af4b2df70ec5c3cf0411b9b0a84
Wilson M. Ramirez F <ramirezwilson20@gmail.com>
2023-12-31 19:07:39 -0300

    add homeWork_3.ts to folder homeWork

--- app.ts
+++ app.ts
@@ -1,6 +1,1 @@
-export let kilograms: number = 10;
- 
- 
-const miles = kilograms * 0.62;
-
-console.log(kilograms + ' kilometros es igual a:',miles, 'millas');
+console.log('Hello word');
---
+++ homeWork/homeWork_3.ts
@@ -0,0 +1,7 @@
+export let base : number = 10;
+ 
+let heigh: number = 5;
+
+let area  = (base * heigh) / 2
+
+console.log('El area del triangulo rectangulo es igual a: ',area);
